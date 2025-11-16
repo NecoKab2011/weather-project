f90b54314083bacfa6652725dbbbd266b6ae4166
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2025-11-16 12:17:14 +0100

    Updates

--- src/components/Hero/HeroTitle/HeroTitle.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./HeroTitle.module.scss"
-
-export const HeroTitle = () => {
-    return(
-        <>
-        <h1 className={styles.hero__title}> Weather dashboard </h1>
-        </>
-    )
-}
